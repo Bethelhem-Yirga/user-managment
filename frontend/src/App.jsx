33a85cb6ac5b61668a9083ce14bd411f68f2197a
Bethelhem-Yirga <bethelhemyirga3@gmail.com>
2026-01-01 14:08:51 +0800

    tailwind

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,43 +1,48 @@
 import { useState } from "react";
 import { createUser, getUser, updateUser, deleteUser } from "./api/user";
+import "./App.css";
 
 function App() {
   const [user, setUser] = useState({ id: "", name: "", email: "" });
   const [result, setResult] = useState(null);
 
-  const handleCreate = async () => {
-    const res = await createUser(user);
-    setResult(res);
-  };
-
-  const handleGet = async () => {
-    const res = await getUser(user.id);
-    setResult(res);
-  };
-
-  const handleUpdate = async () => {
-    const res = await updateUser(user);
-    setResult(res);
-  };
-
-  const handleDelete = async () => {
-    const res = await deleteUser(user.id);
-    setResult(res);
-  };
+  const handleCreate = async () => setResult(await createUser(user));
+  const handleGet = async () => setResult(await getUser(user.id));
+  const handleUpdate = async () => setResult(await updateUser(user));
+  const handleDelete = async () => setResult(await deleteUser(user.id));
 
   return (
-    <div className="App">
-      <h1>User CRUD</h1>
-      <input placeholder="ID" value={user.id} onChange={e => setUser({ ...user, id: e.target.value })} />
-      <input placeholder="Name" value={user.name} onChange={e => setUser({ ...user, name: e.target.value })} />
-      <input placeholder="Email" value={user.email} onChange={e => setUser({ ...user, email: e.target.value })} />
-      <div>
-        <button onClick={handleCreate}>Create</button>
-        <button onClick={handleGet}>Read</button>
-        <button onClick={handleUpdate}>Update</button>
-        <button onClick={handleDelete}>Delete</button>
+    <div className="container">
+<h1 className="text-4xl font-bold text-blue-600 text-center">
+  User CRUD
+</h1>
+
+      <div className="form">
+        <input
+          placeholder="ID"
+          value={user.id}
+          onChange={e => setUser({ ...user, id: e.target.value })}
+        />
+        <input
+          placeholder="Name"
+          value={user.name}
+          onChange={e => setUser({ ...user, name: e.target.value })}
+        />
+        <input
+          placeholder="Email"
+          value={user.email}
+          onChange={e => setUser({ ...user, email: e.target.value })}
+        />
       </div>
-      <pre>{JSON.stringify(result, null, 2)}</pre>
+
+      <div className="buttons">
+        <button className="create" onClick={handleCreate}>Create</button>
+        <button className="read" onClick={handleGet}>Read</button>
+        <button className="update" onClick={handleUpdate}>Update</button>
+        <button className="delete" onClick={handleDelete}>Delete</button>
+      </div>
+
+      <pre className="result">{JSON.stringify(result, null, 2)}</pre>
     </div>
   );
 }
